981804423a78e314a78e082eb775b5ece060e59e
Hammad <hammadyasub14@gmail.com>
2022-04-06 13:17:33 +0530

    create store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
+import movies from './reducers';
 import './index.css';
 import App from './components/App';
+
+const store = createStore(movies);
+
+console.log('store',store);
+console.log('state',store.getState());
+
 
 
 ReactDOM.render(
