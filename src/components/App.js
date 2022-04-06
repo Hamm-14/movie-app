333e5f7290053478201746363799f41918d6634c
Hammad <hammadyasub14@gmail.com>
2022-04-06 18:38:36 +0530

    add functionality to favouite button

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
-import addMovies from '../actions';
+import {addMovies} from '../actions';
 
 
 class App extends React.Component {
@@ -25,9 +25,20 @@ class App extends React.Component {
     // console.log('state after mount',store.getState());
   }
 
+  isMovieFavourite = (movie) => {
+    const {favourites} = this.props.store.getState();
+
+    const index = favourites.indexOf(movie);
+
+    if(index !== -1){
+      return true;
+    }
+    return false;
+  }
+
   render() {
     const {list} = this.props.store.getState();
-    // console.log('from render',this.props.store.getState());
+    console.log('from render',this.props.store.getState());
     return (
       <div className="App">
         <Navbar />
@@ -38,7 +49,12 @@ class App extends React.Component {
           </div>
           <div className="list">
             {list.map((movie,index) => (
-              <MovieCard movie={movie} key={`movies-${index}`}/>
+              <MovieCard   
+                movie={movie} 
+                key={`movies-${index}`} 
+                dispatch={this.props.store.dispatch}
+                isFavourite={this.isMovieFavourite(movie)}
+                />
             ))}
           </div>
         </div>
