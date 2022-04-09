259a9b0f7dffa6d6b2a9ad925f8fe2056f82ba62
Hammad <hammadyasub14@gmail.com>
2022-04-09 09:57:01 +0530

    add functionality to remove from favourites

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {addFavourite} from '../actions';
+import {addToFavourites, removeFromFavourites} from '../actions';
 
 class MovieCard extends React.Component {
 
     handleFavouriteClick = () => {
       const {movie, dispatch} = this.props;
-      dispatch(addFavourite(movie));
+      dispatch(addToFavourites(movie));
     }
 
-    handleUnFavouriteClick = () => {}
+    handleUnFavouriteClick = () => {
+      const {movie, dispatch} = this.props;
+      dispatch(removeFromFavourites(movie));
+    }
 
     render(){
         const {movie,isFavourite} = this.props;
